0aa64d299fd3316bb9f6d6865fa02a3cc4fa1eac
Mohammad Kawadri <62560333+mtkwebdev@users.noreply.github.com>
2024-10-03 13:28:41 +0100

    REST API: A very basic example of dynamic routes

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+example_data = {"data": {"message": "Hello world"}}
+
 @app.get("/")
-def read_api():
+def get_root():
     """Root route, sending an example nested dictionary body of data"""
-    data = {"data": {"message": "Hello world"}}
-    return data
+    return example_data
+
+@app.get("/{name}")
+def get_dynamic_path_by_name(name:str):
+    """A dynamic route that will return a message based on the route path parameter"""
+    copy_of_example_data = example_data
+    copy_of_example_data["data"].update({"message": name})
+    return copy_of_example_data
